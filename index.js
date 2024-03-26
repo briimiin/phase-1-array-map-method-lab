26f2081cc382f8465cb6bf7c20a721e5d1d67e4f
briimiin <kipkorirbrimin005@gmail.com>
2024-03-27 00:25:17 +0300

    array map methods

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(title => {
+    return title.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }).join(' ');
+  });
 }
+
+console.log(titleCased());
